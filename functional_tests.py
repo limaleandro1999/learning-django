b3dea739cca14cc4d9d3ee44e3781e204007dd43
Leandro Lima <leandro.lima@oowlish.com>
2020-03-18 20:21:09 -0300

    refactoring tests

--- functional_tests.py
+++ functional_tests.py
@@ -40,16 +40,24 @@ class NewVisitorTest(unittest.TestCase):
     rows = table.find_elements_by_tag_name('tr')
     self.assertTrue(
       any(row.text == '1: Buy peacock feathers' for row in rows), 
-      "​New to-do item did not appear in table"
+      f'New to-do item did not appear in table. Contents were: {table.text}'
     )
 
-    # There is still a text box inviting her to add another item. She enters "Use peacock feathers to make a fly" (Edith is very methodical)
-    self.fail('Finish the test!')
+    # There is still a text box inviting her to add another item. She enters "Use peacock feathers to make a fly" (Edith is very methodical)  
+    inputbox = self.browser.find_element_by_id('id_new_item')
+    inputbox.send_keys('Use peacock feathers to make a fly')
+    inputbox.send_keys(Keys.ENTER)
+    time.sleep(1)
+
     # The page updates again, and now shows both items on her list
+    table = self.browser.find_element_by_id('id_list_table')
+    rows = table.find_elements_by_tag_name('tr')
+    self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+    self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 
     # Edith wonders whether the site will remember her list. Then she sees that the site has generated a unique URL for her -- there is some
     # explanatory text to that effect.
-
+    self.fail('Finish the test!')
     # She visits that URL - her to-do list is still there.
 
     # Satisfied, she goes back to sleep
