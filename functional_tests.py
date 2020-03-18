65002909e6cabde80fd91859d3bff3bcd30cb05e
Leandro Lima <leandro.lima@oowlish.com>
2020-03-18 20:21:09 -0300

    cherry-picking right refactoring

--- functional_tests.py
+++ functional_tests.py
@@ -40,13 +40,7 @@ class NewVisitorTest(unittest.TestCase):
     # "1: Buy peacock feathers" as an item in a to-do list
     inputbox.send_keys(Keys.ENTER)
     time.sleep(1)
-    
-    table = self.browser.find_element_by_id('id_list_table')
-    rows = table.find_elements_by_tag_name('tr')
-    self.assertTrue(
-      any(row.text == '1: Buy peacock feathers' for row in rows), 
-      f'New to-do item did not appear in table. Contents were: {table.text}'
-    )
+    self.check_for_row_in_list_table('1: Buy peacock feathers')
 
     # There is still a text box inviting her to add another item. She enters "Use peacock feathers to make a fly" (Edith is very methodical)  
     inputbox = self.browser.find_element_by_id('id_new_item')
@@ -55,10 +49,8 @@ class NewVisitorTest(unittest.TestCase):
     time.sleep(1)
 
     # The page updates again, and now shows both items on her list
-    table = self.browser.find_element_by_id('id_list_table')
-    rows = table.find_elements_by_tag_name('tr')
-    self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-    self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+    self.check_for_row_in_list_table('1: Buy peacock feathers')
+    self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
     # Edith wonders whether the site will remember her list. Then she sees that the site has generated a unique URL for her -- there is some
     # explanatory text to that effect.
