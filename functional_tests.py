4c653396e69e084bc1aeab16231fc7c1ebe9cadc
Leandro Lima <leandro.lima@oowlish.com>
2020-03-19 20:34:16 -0300

    Refactoring functional tests

--- functional_tests.py
+++ functional_tests.py
@@ -40,19 +40,6 @@ class NewVisitorTest(unittest.TestCase):
     # "1: Buy peacock feathers" as an item in a to-do list
     inputbox.send_keys(Keys.ENTER)
     time.sleep(1)
-<<<<<<< HEAD
-    self.check_for_row_in_list_table('1: Buy peacock feathers')
-
-    # There is still a text box inviting her to add another item. She enters "Use peacock feathers to make a fly" (Edith is very methodical)  
-    inputbox = self.browser.find_element_by_id('id_new_item')
-    inputbox.send_keys('Use peacock feathers to make a fly')
-    inputbox.send_keys(Keys.ENTER)
-    time.sleep(1)
-
-    # The page updates again, and now shows both items on her list
-    self.check_for_row_in_list_table('1: Buy peacock feathers')
-    self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
-=======
     
     table = self.browser.find_element_by_id('id_list_table')
     rows = table.find_elements_by_tag_name('tr')
@@ -72,7 +59,6 @@ class NewVisitorTest(unittest.TestCase):
     rows = table.find_elements_by_tag_name('tr')
     self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
     self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
->>>>>>> b3dea739cca14cc4d9d3ee44e3781e204007dd43
 
     # Edith wonders whether the site will remember her list. Then she sees that the site has generated a unique URL for her -- there is some
     # explanatory text to that effect.
