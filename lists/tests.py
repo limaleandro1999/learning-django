78bdc5cb50292f40ae041e676e4f117930aeda73
Leandro Lima <leandro.lima@oowlish.com>
2020-03-19 23:07:45 -0300

    Redirect after POST, and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -13,8 +13,15 @@ class HomePageTest(TestCase):
 
   def test_can_save_a_POST_request(self):
     response = self.client.post('/', data={'item_text': 'A new list item'})
-    self.assertIn('A new list item', response.content.decode())
-    self.assertTemplateUsed(response, 'home.html')
+
+    self.assertEqual(Item.objects.count(), 1)
+    new_item = Item.objects.first()
+    self.assertEqual(new_item.text, 'A new list item')
+  
+  def test_redirect_after_POST(self):
+    response = self.client.post('/', data={'item_text': 'A new list item'})
+    self.assertEqual(response.status_code, 302)
+    self.assertEqual(response['location'], '/')
 
   def test_saving_and_retrieving_items(self):
     first_item = Item()
@@ -32,3 +39,16 @@ class HomePageTest(TestCase):
     second_saved_item = saved_items[1]
     self.assertEqual(first_saved_item.text, 'The first (ever) list item')
     self.assertEqual(second_saved_item.text, 'Item the second')
+
+  def test_only_saves_items_when_necessary(self):
+    self.client.get('/')
+    self.assertEqual(Item.objects.count(), 0)
+
+  def test_display_all_list_items(self):
+    Item.objects.create(text='itemey 1')
+    Item.objects.create(text='itemey 2')
+
+    response = self.client.get('/')
+
+    self.assertIn(' itemey 1 ', response.content.decode())
+    self.assertIn(' itemey 2 ', response.content.decode())
