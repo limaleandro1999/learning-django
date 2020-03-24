9931d57d5ef142e52f7c553e375c3c7f8a14de72
Leandro Lima <leandro.lima@oowlish.com>
2020-03-24 11:55:24 -0300

    Creating List model

--- lists/tests.py
+++ lists/tests.py
@@ -3,30 +3,13 @@ from django.test import TestCase
 from django.http import HttpRequest
 
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
 class HomePageTest(TestCase):
   
   def test_uses_home_template(self):
     response = self.client.get('/')
     self.assertTemplateUsed(response, 'home.html')
-
-  def test_saving_and_retrieving_items(self):
-    first_item = Item()
-    first_item.text = 'The first (ever) list item'
-    first_item.save()
-
-    second_item = Item()
-    second_item.text = 'Item the second'
-    second_item.save()
-
-    saved_items = Item.objects.all()
-    self.assertEqual(saved_items.count(), 2)
-    
-    first_saved_item = saved_items[0]
-    second_saved_item = saved_items[1]
-    self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-    self.assertEqual(second_saved_item.text, 'Item the second')
 
   def test_only_saves_items_when_necessary(self):
     self.client.get('/')
@@ -38,9 +21,10 @@ class ListViewTest(TestCase):
     response = self.client.get('/lists/the-only-list-in-the-world/')
     self.assertTemplateUsed(response, 'list.html')
 
-  def test_displays_all_list_items(self):
-    Item.objects.create(text='itemey 1')
-    Item.objects.create(text='itemey 2')
+  def test_displays_all_items(self):
+    list_ = List.objects.create()
+    Item.objects.create(text='itemey 1', list=list_)
+    Item.objects.create(text='itemey 2', list=list_)
 
     response = self.client.get('/lists/the-only-list-in-the-world/')
 
@@ -59,3 +43,32 @@ class NewListTesst(TestCase):
   def test_redirects_after_POST(self):
     response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
     self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+
+class ItemModelTest(TestCase):
+
+  def test_saving_and_retrieving_items(self):
+    list_ = List()
+    list_.save()
+
+    first_item = Item()
+    first_item.text = 'The first (ever) list item'
+    first_item.list = list_
+    first_item.save()
+
+    second_item = Item()
+    second_item.text = 'Item the second'
+    second_item.list = list_
+    second_item.save()
+
+    saved_list = List.objects.first()
+    self.assertEqual(saved_list, list_)
+
+    saved_items = Item.objects.all()
+    self.assertEqual(saved_items.count(), 2)
+
+    first_saved_item = saved_items[0]
+    second_saved_item = saved_items[1]
+    self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+    self.assertEqual(first_saved_item.list, list_)
+    self.assertEqual(second_saved_item.text, 'Item the second')
+    self.assertEqual(second_saved_item.list, list_)
--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect, render
 
-from lists.models import Item
+from lists.models import Item, List
 
 # Create your views here.
 def home_page(request):
@@ -11,5 +11,6 @@ def view_list(request):
   return render(request, 'list.html', {'items': items})
 
 def new_list(request):
-  Item.objects.create(text=request.POST['item_text'])
+  list_ = List.objects.create()
+  Item.objects.create(text=request.POST['item_text'], list=list_)
   return redirect('/lists/the-only-list-in-the-world/')
