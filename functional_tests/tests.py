4acf53040565a783180358d1d13f7ca86f59f2e4
Leandro Lima <leandro.lima@oowlish.com>
2020-03-24 09:36:28 -0300

    new URL, view and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -83,13 +83,12 @@ class NewVisitorTest(LiveServerTestCase):
 
     # She notices that her list has a unique URL
     edith_list_url = self.browser.current_url
-    self.assertRegex(edith_list_url, '/list/.+')
+    self.assertRegex(edith_list_url, '/lists/.+')
 
     # Now a new user, Francis, comes along to the site
 
     ## We use a new browser session to make no information of Edith's is comming through from cookies etc
-    self.browser.quit()
-    self.browser = webdriver.Firefox()
+    self.setUp()
 
     # Francis visits the home page. There is no sign of Edith's list
     self.browser.get(self.live_server_url)
--- lists/tests.py
+++ lists/tests.py
@@ -18,10 +18,10 @@ class HomePageTest(TestCase):
     new_item = Item.objects.first()
     self.assertEqual(new_item.text, 'A new list item')
   
-  def test_redirect_after_POST(self):
+  def test_redirects_after_POST(self):
     response = self.client.post('/', data={'item_text': 'A new list item'})
     self.assertEqual(response.status_code, 302)
-    self.assertEqual(response['location'], '/')
+    self.assertEqual(response['location'], '/lists/the-only-list-in-the-world')
 
   def test_saving_and_retrieving_items(self):
     first_item = Item()
@@ -44,11 +44,17 @@ class HomePageTest(TestCase):
     self.client.get('/')
     self.assertEqual(Item.objects.count(), 0)
 
-  def test_display_all_list_items(self):
+class ListViewTest(TestCase):
+
+  def test_uses_list_template(self):
+    response = self.client.get('/lists/the-only-list-in-the-world')
+    self.assertTemplateUsed(response, 'list.html')
+
+  def test_displays_all_list_items(self):
     Item.objects.create(text='itemey 1')
     Item.objects.create(text='itemey 2')
 
-    response = self.client.get('/')
+    response = self.client.get('/lists/the-only-list-in-the-world')
 
-    self.assertIn('itemey 1', response.content.decode())
-    self.assertIn('itemey 2', response.content.decode())
+    self.assertContains(response, 'itemey 1')
+    self.assertContains(response, 'itemey 2')
