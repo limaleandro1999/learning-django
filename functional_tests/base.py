97ac212d26ed17a069f4af46ff7b3552d2984999
Leandro Lima <leandro.lima@oowlish.com>
2020-03-31 09:21:43 -0300

    Moved Fts into their own individual files

---
+++ functional_tests/base.py
@@ -0,0 +1,34 @@
+import time
+import os
+from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.firefox.options import Options
+
+MAX_WAIT = 10
+
+class FunctionalTest(StaticLiveServerTestCase):
+
+  def setUp(self):
+    options = Options()
+    options.headless = True
+    self.browser = webdriver.Firefox(options=options)
+    staging_server = os.environ.get('STAGING_SERVER')
+    if staging_server:
+      self.live_server_url = 'http://' + staging_server
+  
+  def tearDown(self):
+    self.browser.quit()
+
+  def wait_for_row_in_list_table(self, row_test):
+    start_time = time.time()
+    while True:
+      try:
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_test, [row.text for row in rows])
+        return
+      except(AssertionError, WebDriverException) as e:
+        if time.time() - start_time > MAX_WAIT:
+          raise e
+        time.sleep(0.5)
